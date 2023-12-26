9e99a945df0f78ce4b714efcbd91b376b504bd2d
Pepo Banchev <banchevpepo@gmail.com>
2023-12-26 17:55:32 +0200

    Adding more pages , will implement API later

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Navbar from "../src/Components/Navbar"
 import '../app/globals.css';
+import Login from "@/src/Components/Login";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Login/>
     </>
   )
 
